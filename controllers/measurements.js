514a24728d198d375315259d25b7965cb8178235
valtonv2 <valtonenvaltteri@gmail.com>
2019-11-02 21:18:25 +0200

    Added measurement deletion and test libs

--- controllers/measurements.js
+++ controllers/measurements.js
@@ -11,9 +11,31 @@ measurementRouter.get('/', (request, response) => {
 
 })
 
+//Add new measurement
+
 measurementRouter.post('/', (request, response) => {
 
     dbOps.addOne(request.body).then(added => response.json(added))
+
+})
+
+//Delete a measurement
+
+measurementRouter.delete('/:id', (request, response) => {
+
+  const id = request.params.id
+  dbOps.deleteOne(id).then(response.status(204).end())
+
+})
+
+//Update measurement
+measurementRouter.put('/:id', (request, response) => {
+
+  const id = request.params.id
+  const newMeasurement = request.body
+
+  dbOps.updateOne(id, newMeasurement).then(response.json(newMeasurement))
+
 
 })
 
--- utils/dbutils.js
+++ utils/dbutils.js
@@ -13,7 +13,9 @@ let dbErrorhandler = (err) => {
 
 }
 
+
 //Method for getting all measurements in database
+
 const getAllMeasurements = () => {
     
     let db = new sqlite3.Database(dbPath, dbErrorhandler)
@@ -53,11 +55,74 @@ const addMeasurement = (measurementData) => {
        })
 }
 
+//Method for deleting a measurement by id from database
+
+const deleteMeasurement = (id) => {
+
+    let db = new sqlite3.Database(dbPath, dbErrorhandler)
+    let sql = 'DELETE FROM measurements WHERE id = (?)'
+    console.log(sql)
+    return new Promise((resolve, reject) => {
+        db.run(sql, id, (err) => {
+   
+            db.close()
+            
+            if(err) reject(err)
+            else resolve(id)
+               
+            })
+       })
+}
+
+const updateMeasurement = (id, newData) => {
+
+    let db = new sqlite3.Database(dbPath, dbErrorhandler)
+    console.log('Equality', id === newData.id)
+    let sql = `UPDATE measurements 
+               SET name = '${newData.name}'
+               WHERE id = "(?)"`
+    console.log(sql)
+    
+    return new Promise((resolve, reject) => {
+        db.run(sql, id, function(err){
+   
+            console.log('Changes: ', this.changes)
+            db.close()
+            
+            
+            if(err){ reject(err)}
+            else{
+                console.log('Changes: ', this.changes)
+                resolve(newData)
+            } 
+               
+            })
+       })
+
+}
+
+
+
+
+
+
+
+
+
+
 const dbOps = {
 
     getAll() {return getAllMeasurements()},
     
-    addOne(data) {return addMeasurement(data)}
+    addOne(data) {return addMeasurement(data)},
+
+    deleteOne(id) {return deleteMeasurement(id)},
+
+    updateOne(id, newData) {return updateMeasurement(id, newData)}
+
+
+
+
 
 }
 
